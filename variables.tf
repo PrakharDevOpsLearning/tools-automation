0f87c9b0357a3cb4e1210a186b031d622ec023be
prkhar <prkharpatel20@gmail.com>
2024-03-28 00:48:30 +1100

    corrected variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,20 @@
 variable "tools" {
   default = {
     prometheus = {
-     instance_type = var.instance_type
+     instance_type = "t3.small"
     }
   }
 }
 
 variable "zone_id" {}
-variable "instance_type" {}
+
+
+#variable "tools" {
+#  default = {
+#    prometheus = {
+#      instance_type = var.instance_type
+#    }
+#  }
+#}
+#
+#variable "instance_type" {}
