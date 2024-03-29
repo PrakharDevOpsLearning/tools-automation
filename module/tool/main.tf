4cb8ced92d9247e01b0b8c651aba870bb72256fb
prkhar <prkharpatel20@gmail.com>
2024-03-30 00:34:34 +1100

    added a role for prometheus

--- module/tool/main.tf
+++ module/tool/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "tool" {
   ami                    = data.aws_ami.ami_img.image_id
   vpc_security_group_ids = [data.aws_security_group.sec_grp.id]
   instance_type          = var.instance_type
+#  iam_instance_profile = ""
   tags = {
     Name=var.tool_name
   }
@@ -15,4 +16,26 @@ resource "aws_route53_record" "record" {
   zone_id = var.zone_id
   records = [aws_instance.tool.public_ip]
   ttl     = 30
+}
+
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
 }
