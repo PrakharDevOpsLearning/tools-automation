fd9c91a4d17590a3e0e9da824841a460a3c45365
prkhar <prkharpatel20@gmail.com>
2024-03-30 01:49:20 +1100

    corrected the error of variable of policy_resource-list inside module

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "tool" {
 
   tool_name            = each.key
   instance_type        = each.value["instance_type"]
-  policy_resource_list = each.value["policy_resource_list "]
+  policy_resource_list = each.value["policy_resource_list"]
 
   zone_id       = var.zone_id
 }
