25d93e1aecbb65a15b21802ca7068f01fc0b66d6
prkhar <prkharpatel20@gmail.com>
2024-03-30 00:56:26 +1100

    created and added instance profile

--- module/tool/main.tf
+++ module/tool/main.tf
@@ -4,11 +4,12 @@ resource "aws_instance" "tool" {
   ami                    = data.aws_ami.ami_img.image_id
   vpc_security_group_ids = [data.aws_security_group.sec_grp.id]
   instance_type          = var.instance_type
-#  iam_instance_profile = ""
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
   tags = {
     Name=var.tool_name
   }
 }
+
 
 resource "aws_route53_record" "record" {
   name    = "${var.tool_name}"
@@ -17,6 +18,10 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.tool.public_ip]
   ttl     = 30
 }
+
+#Below code has been added in Session 26, refer session notes why we added -
+
+#IAM role
 
 resource "aws_iam_role" "role" {
   name = "${var.tool_name}-role"
@@ -38,4 +43,11 @@ resource "aws_iam_role" "role" {
   tags = {
     Name = "${var.tool_name}-role"
   }
+}
+
+#Instance Profile
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.tool_name}-role"
+  role = aws_iam_role.role.name
 }
