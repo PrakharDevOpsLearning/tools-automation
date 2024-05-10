84f68d8e2d844a10893e4fb359163682250108c0
prkhar <prkharpatel20@gmail.com>
2024-05-10 23:39:12 +1000

    elk server

--- variables.tf
+++ variables.tf
@@ -4,16 +4,29 @@ variable "tools" {
       instance_type        = "t3.small"
       policy_resource_list = ["ec2:DescribeInstances"] #session 26
     }
+
     grafana = {
       instance_type        = "t3.small"
       policy_resource_list = []
     }
+
     vault = {
       instance_type        = "t3.small"
       policy_resource_list = []
     }
+
     ci = {
       instance_type        = "t3.small"
+      policy_resource_list = []
+    }
+
+    sonarqube = {
+      instance_type        = "t3.large"
+      policy_resource_list = []
+    }
+
+    elk = {
+      instance_type        = "r7i.large"
       policy_resource_list = []
     }
 
